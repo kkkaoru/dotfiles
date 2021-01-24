8a20002adeb919fd5a147731d545935594efd2c7
kaoru.ohtaka <ockacru+git@gmail.com>
2021-01-24 10:39:57 +0900

    Update fish conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,16 +5,31 @@ end
 
 # Envs
 set -x SHELL /usr/local/bin/fish
-set -x PATH /usr/local/opt/openssl/bin $PATH
 set -x LDFLAGS "-L/usr/local/opt/openssl/lib"
 set -x CPPFLAGS "-I/usr/local/opt/openssl/include"
+set -x ANDROID_HOME /usr/local/share/android-sdk
+
+set -x PATH /usr/local/opt/openssl/bin $PATH
+set -x PATH /usr/local/opt/mysql@5.7/bin $PATH
+set -x PATH $PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools $PATH
 
 # Evals
 eval (thefuck --alias | source)
 eval (anyenv init - | source)
+eval (direnv hook fish)
 
 # Aliases
 alias ghq-cd='cd (ghq root)/(ghq list | peco)'
-alias git-checkout-local='git checkout (git branch|peco)'
 alias source-fish='exec $SHELL -l'
-alias youtube='mpsyt'
+# for git
+alias git-checkout-local='bash -c \'git checkout $(git branch | peco)\''
+alias git-branch-clean='git checkout master && git branch --merged | grep -v -e master | xargs git branch -d'
+alias git-push-origin='git push -u origin head'
+alias wget-static='wget --page-requisites --html-extension --convert-links'
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
+set -gx LDFLAGS "-L/usr/local/opt/icu4c/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/icu4c/include"
+set -gx PKG_CONFIG_PATH "/usr/local/opt/icu4c/lib/pkgconfig"
+set -g fish_user_paths "/usr/local/opt/gnu-getopt/bin" $fish_user_paths
