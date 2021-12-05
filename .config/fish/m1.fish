81a6bc78d73354bdb146d81adeb7795fc27405ac
kaoru.ohtaka <ockacru+git@gmail.com>
2021-12-05 19:16:24 +0900

    use anyenv

--- .config/fish/m1.fish
+++ .config/fish/m1.fish
@@ -1,13 +1,3 @@
-# Evals
-# Disabled anyenv
-# eval (anyenv init - | source)
-
-# asdf
-# m1 mac
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
-# intel mac
-# source /usr/local/opt/asdf/libexec/asdf.fish
-
 eval (/opt/homebrew/bin/brew shellenv)
 
 source (dirname (status -f))/aliases.fish
@@ -15,3 +5,10 @@ source (dirname (status -f))/envs.fish
 source (dirname (status -f))/binds.fish
 
 eval (direnv hook fish | source)
+eval (anyenv init - | source)
+
+# asdf
+# m1 mac
+# source /opt/homebrew/opt/asdf/libexec/asdf.fish
+# intel mac
+# source /usr/local/opt/asdf/libexec/asdf.fish
