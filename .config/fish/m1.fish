6958fab44101cb7a3b50c5720e65b8f418745ddc
kaoru.ohtaka <ockacru+git@gmail.com>
2021-12-05 18:57:24 +0900

    can use direnv

--- .config/fish/m1.fish
+++ .config/fish/m1.fish
@@ -13,3 +13,5 @@ eval (/opt/homebrew/bin/brew shellenv)
 source (dirname (status -f))/aliases.fish
 source (dirname (status -f))/envs.fish
 source (dirname (status -f))/binds.fish
+
+eval (direnv hook fish | source)
