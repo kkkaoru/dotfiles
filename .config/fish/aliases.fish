4748ef62fb20a54f0554cc700b2fb143fca86e66
kaoru.ohtaka <ockacru+git@gmail.com>
2021-12-05 18:53:39 +0900

    split fish config

---
+++ .config/fish/aliases.fish
@@ -0,0 +1,8 @@
+# Aliases
+alias ghq-cd='cd (ghq root)/(ghq list | peco)'
+alias source-fish='exec $SHELL -l'
+# for git
+alias git-checkout-local='bash -c \'git checkout $(git branch | peco)\''
+alias git-branch-clean='git checkout master && git branch --merged | grep -v -e master | xargs git branch -d'
+alias git-push-origin='git push -u origin head'
+alias wget-static='wget --page-requisites --html-extension --convert-links'
---
+++ .config/fish/envs.fish
@@ -0,0 +1,12 @@
+set -x LDFLAGS -L/usr/local/opt/openssl/lib
+set -x CPPFLAGS -I/usr/local/opt/openssl/include
+set -x ANDROID_HOME /usr/local/share/android-sdk
+set -x PATH /usr/local/opt/openssl/bin $PATH
+set -x PATH $PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools $PATH
+set -g fish_user_paths /usr/local/sbin $fish_user_paths
+set -g fish_user_paths /usr/local/opt/icu4c/bin $fish_user_paths
+set -g fish_user_paths /usr/local/opt/icu4c/sbin $fish_user_paths
+set -gx LDFLAGS -L/usr/local/opt/icu4c/lib
+set -gx CPPFLAGS -I/usr/local/opt/icu4c/include
+set -gx PKG_CONFIG_PATH /usr/local/opt/icu4c/lib/pkgconfig
+set -g fish_user_paths /usr/local/opt/gnu-getopt/bin $fish_user_paths
