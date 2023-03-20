d824e58042f243c64d46285165a933b4fb6a1dac
kaoru.ohtaka <ockacru+git@gmail.com>
2023-03-21 01:57:48 +0900

    add deno

--- init-asdf.sh
+++ init-asdf.sh
@@ -4,4 +4,5 @@ asdf plugin-add nodejs
 asdf install nodejs latest
 asdf global nodejs latest
 asdf plugin-add direnv
-
+asdf plugin-add deno https://github.com/asdf-community/asdf-deno.git
+asdf install deno latest
