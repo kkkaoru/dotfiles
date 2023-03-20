2da8f77617340e4b66e8a88466ee0fd0cd5e1f36
kaoru.ohtaka <ockacru+git@gmail.com>
2023-03-21 01:52:10 +0900

    add pnpm home

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,8 @@ source (dirname (status -f))/aliases.fish
 source (dirname (status -f))/envs.fish
 source (dirname (status -f))/binds.fish
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# pnpm
+set -gx PNPM_HOME $HOME/Library/pnpm
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
