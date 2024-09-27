47ff6ea362ef6de6960bef8e55907faecc696510
kaoru.ohtaka <ockacru+git@gmail.com>
2024-09-27 12:32:35 +0900

    add mise and remove asdf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,12 +4,5 @@ source (dirname (status -f))/aliases.fish
 source (dirname (status -f))/envs.fish
 source (dirname (status -f))/binds.fish
 source (dirname (status -f))/path.fish
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
-# pnpm
-set -gx PNPM_HOME "/Users/kaoru/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
 set -gx HOMEBREW_GITHUB_API_TOKEN your_token_here
--- init-asdf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-asdf plugin-add nodejs
-asdf install nodejs latest
-asdf global nodejs latest
-asdf plugin-add direnv
-asdf plugin-add deno https://github.com/asdf-community/asdf-deno.git
-asdf install deno latest
