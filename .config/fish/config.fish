b924b37f22b5e1668fd1b29d2c93cfd710e04fce
kaoru.ohtaka <ockacru+git@gmail.com>
2025-07-31 04:18:50 +0900

    temp

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,20 @@ source (dirname (status -f))/binds.fish
 source (dirname (status -f))/path.fish
 
 set -gx HOMEBREW_GITHUB_API_TOKEN your_token_here
+
+# pnpm
+# set -gx PNPM_HOME /Users/kaoru/Library/pnpm
+# if not string match -q -- $PNPM_HOME $PATH
+#     set -gx PATH "$PNPM_HOME" $PATH
+# end
+# pnpm end
+# set -gx PNPM_HOME /Users/kaoru/.local/share/mise/installs/node/18/bin/pnpm
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+
+# Added by Windsurf
+fish_add_path /Users/kaoru/.codeium/windsurf/bin
+
+# thefuck alias
+thefuck --alias | source
