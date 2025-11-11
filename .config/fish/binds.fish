9213969b142483e0a3505f49c9a18b861023f563
kkk4oru <kaoru+git@teadea.net>
2025-11-11 10:38:03 +0900

    feat(fish): enable fzf powered history search

--- .config/fish/binds.fish
+++ .config/fish/binds.fish
@@ -1,5 +1,10 @@
 # history
+function fzf_select_history
+    history | fzf --query="$commandline" | read -l result
+    and commandline -r $result
+end
+
 function fish_user_key_bindings
-    # bind \cr fzf_select_history
-    bind \cr peco_select_history
+    bind \cr fzf_select_history
+    # bind \cr peco_select_history
 end
